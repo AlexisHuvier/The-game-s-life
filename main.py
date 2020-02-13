d69f5fb1406b71925eed416f7b78fe06519558b7
Alexis Gridel <50674490+Inerska@users.noreply.github.com>
2020-02-13 18:56:47 +0100

    No crash and grid creation
    Co-Authored-By: clems3 <clems3@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import sys
 from grid import Grid
 import os
 
@@ -7,16 +8,44 @@ os.environ['SDL_VIDEO_CENTERED'] = str(1)
 
 
 def main():
+    line_wth = 2
+    square_wth = 10
+    square_hght = 10
+    table_li_grid = [[],[]]
     pg.init()
     clock = pg.time.Clock()
     win = pg.display.set_mode(ratio)
-    pg.display.set_caption(f"The game's life | {round(clock.get_fps())} FPS")
+    pg.display.set_caption("The game's life | {} FPS".format(round(clock.get_fps())))
     grid = Grid()
     grid.__init__()
     win.fill((255, 255, 255))
+    for row in range(WIDTH):
+        table_li_grid.append([])
+        for column in range(HEIGHT):
+            table_li_grid[row].append(0)
+    for row in range(WIDTH):
+        for column in range(HEIGHT):
+            color = (0,0,0)
+            if table_li_grid[row][column] == 1:
+                color = (255,255,255)
+            pg.draw.rect(win,(0,0,0), [(line_wth + square_wth) * column + line_wth, (line_wth + square_hght) * row + line_wth, square_wth, square_hght])
+
+
     while True:
         for _ in pg.event.get():
-            if _.type == pg.QUIT: return
+            key = pg.key.get_pressed()
+            if key[pg.K_ESCAPE] or _.type == pg.QUIT:
+                pg.quit()
+                sys.exit(0)
+            elif _.type == pg.MOUSEBUTTONDOWN:
+                pos = pg.mouse.get_pos()
+
+                column = pos[0] // (square_wth + line_wth)
+                row = pos[1] // (square_hght + line_wth)
+
+                table_li_grid[row][column] = 1
+                print("Click ", pos, "Grid coordinates: ", row, column)
+
         grid.display()
         pg.display.update()
 
