9ca09d9b46640b370246f9123d764c70a2c45527
Alexis Gridel <50674490+Inerska@users.noreply.github.com>
2020-02-24 12:04:18 +0100

    OOP convention

--- grid.py
+++ grid.py
@@ -11,13 +11,21 @@ class Grid:
         for row in range(50):
             for column in range(67):
                 pg.draw.rect(_win, (255, 255, 255),
-                             [(self.m_line_wth + self.m_square_wth) * column + self.m_line_wth, (self.m_line_wth + self.m_square_hght) * row + self.m_line_wth,
-                              self.m_square_wth, self.m_square_hght])
+                             [(self.getLine_wth() + self.getSquare_wth()) * column + self.getLine_wth(), (self.getLine_wth() + self.getSquare_hght()) * row + self.getLine_wth(),
+                              self.getSquare_wth(), self.getSquare_hght()])
 
-    def selectACell(self):
+    @classmethod
+    def selectACell(cls):
         pos = pg.mouse.get_pos()
-        column = pos[0] // (self.m_square_wth + self.m_line_wth)
-        row = pos[1] // (self.m_square_hght + self.m_line_wth)
-
-        self.m_grid[row][column] = 1
+        column = pos[0] // (Grid.m_square_wth + Grid.m_line_wth)
+        row = pos[1] // (Grid.m_square_hght + Grid.m_line_wth)
+        cls.m_grid[row][column] = 1
         print("Click ", pos, "Grid coordinates: ", row, column)
+
+    def getLine_wth(self): return self.m_line_wth
+    def getSquare_wth(self): return self.m_square_wth
+    def getSquare_hght(self): return self.m_square_hght
+    def setLine_wth(self, value): self.m_line_wth = value
+    def setSquare_wth(self, value): self.m_square_wth = value
+    def setSquare_hght(self, value): self.m_square_hght = value
+
