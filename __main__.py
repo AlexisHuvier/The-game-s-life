1e7cb9c28ebd8d0e627a58abdd01f4810542f33e
Alexis Gridel <50674490+Inerska@users.noreply.github.com>
2020-02-24 10:03:41 +0100

    More optimized

--- __main__.py
+++ __main__.py
@@ -8,27 +8,12 @@ os.environ['SDL_VIDEO_CENTERED'] = str(1)
 
 
 def main():
-    line_wth = 2
-    square_wth = 10
-    square_hght = 10
-    table_li_grid = [[], []]
     pg.init()
     clock = pg.time.Clock()
     win = pg.display.set_mode(ratio)
     pg.display.set_caption("The game's life | {} FPS".format(round(clock.get_fps())))
-    grid = Grid()
-    grid.__init__()
     win.fill((0, 0, 0))
-    for row in range(50):
-        table_li_grid.append([])
-        for column in range(67):
-            table_li_grid[row].append(0)
-    for row in range(50):
-        for column in range(67):
-            pg.draw.rect(win, (255, 255, 255),
-                         [(line_wth + square_wth) * column + line_wth, (line_wth + square_hght) * row + line_wth,
-                          square_wth, square_hght])
-
+    grid = Grid(win)
     while True:
         for _ in pg.event.get():
             key = pg.key.get_pressed()
@@ -36,15 +21,8 @@ def main():
                 pg.quit()
                 sys.exit(0)
             elif _.type == pg.MOUSEBUTTONDOWN:
-                pos = pg.mouse.get_pos()
+                grid.selectACell()
 
-                column = pos[0] // (square_wth + line_wth)
-                row = pos[1] // (square_hght + line_wth)
-
-                table_li_grid[row][column] = 1
-                print("Click ", pos, "Grid coordinates: ", row, column)
-
-        grid.display()
         pg.display.update()
 
 
--- grid.py
+++ grid.py
@@ -1,6 +1,23 @@
-class Grid:
-    m_grid = []
+import pygame as pg
 
-    @staticmethod
-    def display():
-        pass
+
+class Grid:
+    m_line_wth = 2
+    m_square_wth = 10
+    m_square_hght = 10
+    m_grid = [[0 for column in range(67)] for row in range(50)]
+
+    def __init__(self, _win):
+        for row in range(50):
+            for column in range(67):
+                pg.draw.rect(_win, (255, 255, 255),
+                             [(self.m_line_wth + self.m_square_wth) * column + self.m_line_wth, (self.m_line_wth + self.m_square_hght) * row + self.m_line_wth,
+                              self.m_square_wth, self.m_square_hght])
+
+    def selectACell(self):
+        pos = pg.mouse.get_pos()
+        column = pos[0] // (self.m_square_wth + self.m_line_wth)
+        row = pos[1] // (self.m_square_hght + self.m_line_wth)
+
+        self.m_grid[row][column] = 1
+        print("Click ", pos, "Grid coordinates: ", row, column)
