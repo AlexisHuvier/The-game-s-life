783eb641c4fd9f91b138ff4b8ff88fa6c7048c6c
Alexis Gridel <50674490+Inerska@users.noreply.github.com>
2020-03-13 22:10:06 +0100

    generate_life() * 100

--- Basic_version.py
+++ Basic_version.py
@@ -1,3 +1,5 @@
+from random import randint
+
 import pygame as pg
 import os
 
@@ -10,7 +12,16 @@ def draw(_line_wth, _square_wth):
                           (_line_wth + _square_wth) * row + _line_wth,
                           _square_wth, _square_wth])
             print(row, column)
-            #cells_table[row][column] = False
+            # cells_table[row][column] = False
+
+
+def generate_life(_win, _line_wth, _square_wth):
+    random_cell_r, random_cell_c = randint(0, 49), randint(0, 66)
+    pg.draw.rect(_win, (42, 204, 113), (
+        pg.Rect(random_cell_c * _square_wth + _line_wth * (random_cell_c + 1),
+                random_cell_r * _square_wth + _line_wth * (random_cell_r + 1), _square_wth,
+                _square_wth)))
+
 
 
 def select_cells():
@@ -23,7 +34,7 @@ cells_table = [[], []]
 line_wth = 2
 square_wth = 10
 draw(line_wth, square_wth)
-
+[generate_life(win, 2, 10) for _ in range(100)]
 continuer = True
 while continuer:
     pg.display.update()
