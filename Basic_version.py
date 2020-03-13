abd9b2234e67e0a657323f523e8c0caf2186341c
Alexis Gridel <50674490+Inerska@users.noreply.github.com>
2020-03-13 22:05:22 +0100

    [fix] shadow variable conflict

--- Basic_version.py
+++ Basic_version.py
@@ -1,30 +1,31 @@
 import pygame as pg
 import os
 
-def draw (line_wth, square_wth):
+
+def draw(_line_wth, _square_wth):
     for row in range(50):
         for column in range(67):
             pg.draw.rect(win, (52, 73, 94),
-                         [(line_wth + square_wth) * column + line_wth,
-                          (line_wth + square_wth) * row + line_wth,
-                          square_wth, square_wth])
+                         [(_line_wth + _square_wth) * column + _line_wth,
+                          (_line_wth + _square_wth) * row + _line_wth,
+                          _square_wth, _square_wth])
             print(row, column)
-            cells_table[row][column] = False
+            #cells_table[row][column] = False
 
 
 def select_cells():
     pass
 
+
 pg.init()
-win = pg.display.set_mode((800,600))
+win = pg.display.set_mode((800, 600))
 cells_table = [[], []]
 line_wth = 2
 square_wth = 10
 draw(line_wth, square_wth)
 
-
 continuer = True
-while continuer :
+while continuer:
     pg.display.update()
     for _ in pg.event.get():
         key = pg.key.get_pressed()
