28d23e10af57f03cff60b757be9a6952c61a447c
Alexis Gridel <50674490+Inerska@users.noreply.github.com>
2020-03-14 11:29:39 +0100

    [opti] #25, 30, 34 bool

--- Basic_version.py
+++ Basic_version.py
@@ -15,41 +15,36 @@ def draw(_line_wth, _square_wth):
             cells_table[row, column] = False
 
 
-
-
-
-
-
 def select_cells():
-
     pos = pg.mouse.get_pos()
     column = pos[0] // (square_wth + line_wth)
     row = pos[1] // (square_wth + line_wth)
     cells_table[row, column] = True
     print(row, column, cells_table[row, column])
 
-    if cells_table[row, column] == True:
+    if cells_table[row, column]:
         pg.draw.rect(win, (42, 204, 113), (
             pg.Rect(column * square_wth + line_wth * (column + 1),
                     row * square_wth + line_wth * (row + 1), square_wth,
                     square_wth)))
-    if cells_table[row, column] == False:
+    if not cells_table[row, column]:
         pg.draw.rect(win, (52, 73, 94), (
             pg.Rect(column * square_wth + line_wth * (column + 1),
                     row * square_wth + line_wth * (row + 1), square_wth,
-                    square_wth())))
+                    square_wth)))
 
-def life() :
+
+def life():
     for row in range(1, 49):
         for column in range(1, 66):
 
             neighbours_count = 0
             for x in range(-1, 2):
                 for y in range(-1, 2):
-                    if cells_table[row + x, column + y] == True:
+                    if cells_table[row + x, column + y]:
                         neighbours_count += 1
                     # print(neighbours_count)
-            #neighbours_count -= 1
+            # neighbours_count -= 1
             if cells_table[row, column] == True and (neighbours_count < 2 or neighbours_count > 3):
                 cells_table[row, column] = False
             elif cells_table[row, column] == True and (neighbours_count == 2 or neighbours_count == 3):
@@ -80,14 +75,13 @@ print(cells_table)
 continuer = True
 while continuer:
 
-
     pg.display.update()
     for _ in pg.event.get():
         key = pg.key.get_pressed()
         if key[pg.K_ESCAPE] or _.type == pg.QUIT:
             pg.quit()
             os.sys.exit(0)
-        if key[pg.K_p] :
+        if key[pg.K_p]:
             gen = 0
             while 1:
                 life()
