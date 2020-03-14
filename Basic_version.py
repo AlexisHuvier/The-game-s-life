0954c09361deec96e3124c4e4be060001bb22ab0
Alexis Gridel <50674490+Inerska@users.noreply.github.com>
2020-03-14 11:31:58 +0100

    [opti] #45, 47, 49, 51, 56 bool

--- Basic_version.py
+++ Basic_version.py
@@ -37,26 +37,23 @@ def select_cells():
 def life():
     for row in range(1, 49):
         for column in range(1, 66):
-
             neighbours_count = 0
             for x in range(-1, 2):
                 for y in range(-1, 2):
                     if cells_table[row + x, column + y]:
                         neighbours_count += 1
-                    # print(neighbours_count)
-            # neighbours_count -= 1
-            if cells_table[row, column] == True and (neighbours_count < 2 or neighbours_count > 3):
+            if cells_table[row, column] and (neighbours_count < 2 or neighbours_count > 3):
                 cells_table[row, column] = False
-            elif cells_table[row, column] == True and (neighbours_count == 2 or neighbours_count == 3):
+            elif cells_table[row, column] and (neighbours_count == 2 or neighbours_count == 3):
                 cells_table[row, column] = True
-            elif cells_table[row, column] == False and neighbours_count == 3:
+            elif cells_table[row, column] and neighbours_count == 3:
                 cells_table[row, column] = True
-            if cells_table[row, column] == True:
+            if cells_table[row, column]:
                 pg.draw.rect(win, (42, 204, 113), (
                     pg.Rect(column * square_wth + line_wth * (column + 1),
                             row * square_wth + line_wth * (row + 1), square_wth,
                             square_wth)))
-            if cells_table[row, column] == False:
+            if not cells_table[row, column]:
                 pg.draw.rect(win, (52, 73, 94), (
                     pg.Rect(column * square_wth + line_wth * (column + 1),
                             row * square_wth + line_wth * (row + 1), square_wth,
